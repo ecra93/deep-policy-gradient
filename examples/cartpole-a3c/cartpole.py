8814561741f6dc2af07b4f700d01aebf0071ac0f
Evan <ecra93@gmail.com>
2018-09-17 00:15:49 +1000

    [Evan] Prototype A3C with CartPole

---
+++ examples/cartpole-a3c/cartpole.py
@@ -0,0 +1,62 @@
+import tensorflow as tf
+import time
+
+from network import Network, Optimizer
+from worker import Worker
+
+
+if __name__ == "__main__":
+
+    n_actions = 2
+    n_episodes = 500
+    n_workers = 3
+    n_optimizers = 2
+    game = "CartPole-v1"
+
+    with tf.Session() as sess:
+
+        # create master
+        print("Initializing network ...")
+        network = Network(n_actions, sess)
+
+        # create workers
+        print("Initializing workers ...")
+        workers = []
+        for _ in range(n_workers):
+            worker = Worker(game, network, n_episodes)
+            workers.append(worker)
+
+        # start each of the workers
+        for i in range(1, n_workers):
+            workers[i].start()
+
+        # start an optimizer
+        print("Initializing optimizers ...")
+        optimizers = []
+        for i in range(n_optimizers):
+            optimizer = Optimizer(network)
+            optimizers.append(optimizer)
+        
+        # start each of the optimizers
+        for i in range(n_optimizers):
+            optimizers[i].start()
+
+        # run worker[0] on main thread
+        workers[0].is_main = True
+        workers[0].run()
+
+        # wait for the remaining workers to finish
+        for i in range(1, n_workers):
+            workers[i].join()
+        print("... all workers closed.")
+
+        # wait till all network episodes have been used for training
+        while network.episodes:
+            time.sleep(10)
+        for i in range(n_optimizers):
+            optimizers[i].stop = True
+
+        # wait for the optimizer to wrap up
+        for i in range(n_optimizers):
+            optimizers[i].join()
+        print("... all optimizers closed.")
